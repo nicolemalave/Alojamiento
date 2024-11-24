5155fa6fc4616f8580639ed1d9153837b4693841
cardisu <cardisukirit@hotmail.com>
2024-11-24 14:32:34 -0500

    Proyecto Alojamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule,Routes } from '@angular/router';
+import { CRUDComponent } from './crud/crud.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'crud', component: CRUDComponent },
+    { path: '', redirectTo: '/crud', pathMatch: 'full' }, // Ruta por defecto
+  // Puedes agregar más rutas aquí según sea necesario
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
